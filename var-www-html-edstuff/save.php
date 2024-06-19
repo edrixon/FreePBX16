4992f1ed37a2c77be9c948d7788b6113bc5a005f
edrixon <69010489+edrixon@users.noreply.github.com>
2024-06-19 08:15:46 +0100

    Add files via upload

---
+++ var-www-html-edstuff/save.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Phone line configuration</title>
+
+  <link rel="stylesheet" href="styles.css"
+
+</head>
+
+<body>
+
+<div class="all">
+
+  <hr>
+  <div class="header"><h2>Yealink phone configuration</h2></div>
+  <hr>
+
+  <div class="main">
+
+    <br>
+
+    <?php
+
+        $mac=$_POST["mac"];
+        $extn1=$_POST["extn1"];
+        $extn2=$_POST["extn2"];
+
+        $cmd="/usr/local/bin/mkphone" . " " . $mac . " " . $extn1 . " " . $extn2;
+
+        exec($cmd, $output, $result);
+        if ($result ==  0) {
+            echo "<p>Generated configuration file</p>";
+        }
+        else
+        {
+            echo "<p>Failed to generate configuration file</p>";
+        }
+
+    ?>
+
+    <form action="phoneconfig.html">
+      <input class="button" type="submit" value="OK">
+    </form>
+
+    <br>
+
+  </div>
+
+  <hr>
+  <div class="footer">Ed Rixon, June 2024</div>
+  <hr>
+
+</div>
+
+</body>
+
+</html>
