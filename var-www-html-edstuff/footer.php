907defd6ee12848647ab95f62a31e97633315ca2
edrixon <69010489+edrixon@users.noreply.github.com>
2024-06-25 12:03:07 +0100

    Add files via upload

---
+++ var-www-html-edstuff/footer.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Phone line configuration</title>
+
+  <link rel="stylesheet" href="styles.css"
+
+</head>
+
+<body>
+
+  <hr>
+    <div class="footer">Ed Rixon, June 2024</div>
+  <hr>
+
+
+</body>
+
+</html>
